d9a61d4b523b7a46bb3ca4f6808a07b8a39ebf2d
SebastyanDev <sepineda@unicesar.edu.co>
2024-02-19 10:54:08 -0500

    cambios

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -12,7 +12,7 @@
 
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta  name="keywords" content="Intesa, instituto tecnico del saber, carreras tecnicas, carreras tecnicas barrancabermeja, estudiar virtual, escuela barrancabermeja.">
-    <meta name="description" content="Institución tecnico laboral para el trabajo y el desarrollo humano." >
+    <meta name="description" content="Institución tecnico laboral para el trabajo y el desarrollo humano; con licencia de funcionamiento según resolución 3121 del 14 de diciembre 2015. Ofrecemos capacitación de nivel técnico, diplomados, cursos y seminarios. Nos identificamos con la política de calidad y mejoramiento continuo; brindando así, una formación acorde a las necesidades laborales, conforme a los avances tecnológicos del mercado" >
     <meta name="author" content="Instituto Tecnico Del Saber">
     <meta property="og:locale" content="es_ES" />
     <meta property="og:type" content="website" />
