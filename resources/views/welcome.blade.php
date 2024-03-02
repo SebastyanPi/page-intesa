5aa1eeeaebdecbd8712755d2b946dd2ee62ed52b
SebastyanDev <sepineda@unicesar.edu.co>
2024-03-02 12:49:49 -0500

    Subiendo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,22 @@
                         <div class="col-md-6 col-lg-3 text-center team mb-4">
                             <div class="team-item rounded overflow-hidden mb-2">
                                 <div class="team-img position-relative">
+                                    <img class="img-fluid" src="{{ env('APP_URL') }}img/Preicfes.png" alt="">
+                                    <div class="team-social">
+                                        <a class="btn btn-outline-light btn-square mx-1" href="https://wa.link/e97v10"><i class="fa fa-whatsapp"></i></a>
+                                        <a class="btn btn-outline-light btn-square mx-1" href="https://www.facebook.com/INTESABARRANCABERMEJA2018"><i class="fab fa-facebook-f"></i></a>
+                                        <a class="btn btn-outline-light btn-square mx-1 d-none" href="#"><i class="fab fa-linkedin-in"></i></a>
+                                    </div>
+                                </div>
+                                <div class="bg-secondary p-4">
+                                    <h5>Examen Preicfes</h5>
+                                    <p class="m-0"><a href="https://presaber.institutointesa.edu.co/" class="btn btn-primary btn-sm">Mas información!</button></a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3 text-center team mb-4">
+                            <div class="team-item rounded overflow-hidden mb-2">
+                                <div class="team-img position-relative">
                                     <img class="img-fluid" src="{{ env('APP_URL') }}img/enterate/1.jpg" alt="">
                                     <div class="team-social">
                                         <a class="btn btn-outline-light btn-square mx-1" href="https://wa.link/e97v10"><i class="fa fa-whatsapp"></i></a>
